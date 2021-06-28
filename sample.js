f4fe5109f5c14cd027e7be1687a8648d27a4d6e6
Minhtam Nguyen <minhtam_nguyen@yahoo.com>
2021-06-28 00:24:09 -0700

    Changed code to accept page number via parameter and print the content of the object at data[0].

--- sample.js
+++ sample.js
@@ -1,15 +1,14 @@
 const axios = require("axios");
 
-const getArticle = async () => {
-  const pageNum = 2;
+const getArticle = async (pageNum) => {
   let page = (await axios.get("https://jsonmock.hackerrank.com/api/articles?page=" + pageNum)).data;
   console.log(`page: ${page.page}`);
   console.log(`total_pages: ${page.total_pages}`);
-  console.log(`data[0]: ${page.data[0]}`);
+  console.log('data[0]:', page.data[0]);
 };
 
 if (module === require.main) {
-  getArticle()
+  getArticle(2)
     .then(() => console.log(`\nREST API query completed successfully\n`))
     .catch(err => console.error(err.message));
 }
